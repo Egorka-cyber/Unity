0ee5d1086dfa853096adc359bb5e5eb250080753
Egorka-cyber <egortalko0@gmail.com>
2021-10-23 16:30:53 +0300

    Unity

--- CubeCollections.cs
+++ CubeCollections.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeCollections : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] cubes;
+    [SerializeField] private Text inputText;
+
+    public void DeleteCube()
     {
-        
+        Destroy(cubes[int.Parse(inputText.text)]);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PrintCube()
     {
-        
+        print(cubes[int.Parse(inputText.text)]);
     }
 }
