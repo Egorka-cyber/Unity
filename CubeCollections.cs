d1e20c3792e832e032d07e313e12dc5bcab326c6
Egorka-cyber <egortalko0@gmail.com>
2021-10-26 20:00:55 +0300

    Unity

--- CubeCollections.cs
+++ CubeCollections.cs
@@ -5,12 +5,13 @@ using UnityEngine.UI;
 
 public class CubeCollections : MonoBehaviour
 {
-    [SerializeField] private GameObject[] cubes;
+    [SerializeField] private List<GameObject> cubes;
     [SerializeField] private Text inputText;
 
     public void DeleteCube()
     {
         Destroy(cubes[int.Parse(inputText.text)]);
+        cubes.RemoveAt(int.Parse(inputText.text));
     }
 
     public void PrintCube()
