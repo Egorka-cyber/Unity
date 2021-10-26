befad58221e284379e98a38e52c90b32e1a16f8d
Egorka-cyber <egortalko0@gmail.com>
2021-10-26 20:16:44 +0300

    Unity

--- Test.cs
+++ Test.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public List<int> numbers = new List<int>() {1, 2, 3, 4, 5, 6, 7};
 
+    private void Start()
+    {
+        numbers.RemoveAt(0);
+        numbers.Add(2);
+
+        foreach (int numbers in numbers);
+        {
+            print(numbers);
+        }
+    }
 }
